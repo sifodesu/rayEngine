8c750571a83de313e411a31ac27913fc291c9455
Sifodias <coronel.ilan@gmail.com>
2021-09-03 14:57:04 +0300

    bugfix

--- src/runes.cpp
+++ src/runes.cpp
@@ -7,22 +7,27 @@ std::vector<Sprite*> Runes::sprites_;
 void Runes::init() {
     for (int rune = 0; rune < 4; rune++) {
         sprites_.push_back(new Sprite(std::to_string(rune+1) + ".png", 1));
-        queue_.push_back(GetRandomValue(1, 4));
+        newRune();
     }
 }
 
 void Runes::newRune() {
-    int front = queue_.front();
-    queue_.pop_front();
-    int back = front;
-    while (back == front)
+    if (!queue_.size()){
+        queue_.push_back(GetRandomValue(1, 4));
+        return;
+    }
+    int last = queue_.back();
+    int back = last;
+    while (back == last)
         back = GetRandomValue(1, 4);
     queue_.push_back(back);
 }
 
 void Runes::routine() {
-    if (InputMap::checkPressed("r" + std::to_string(queue_.front())))
+    if (InputMap::checkPressed("r" + std::to_string(queue_.front()))){
+        queue_.pop_front();
         newRune();
+    }
 }
 
 void Runes::draw(Vector2 pos) {
